a851e517c5c11cbe2549eaa27c4828041fab6e0e
Andre-21901850 <61785546+Andre-21901850@users.noreply.github.com>
2026-05-18 13:01:00 +0000

    Adicionados modelos Licenciatura e Docente com dados no admin

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Licenciatura, Docente
 
-# Register your models here.
+@admin.register(Licenciatura)
+class LicenciaturaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'instituicao', 'ano_inicio', 'ano_fim')
+    search_fields = ('nome', 'instituicao')
+
+@admin.register(Docente)
+class DocenteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email')
+    search_fields = ('nome', 'email')
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Licenciatura(models.Model):
+    nome = models.CharField(max_length=200)
+    instituicao = models.CharField(max_length=200)
+    ano_inicio = models.IntegerField()
+    ano_fim = models.IntegerField(null=True, blank=True)
+    descricao = models.TextField(blank=True)
+    link = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.nome
+
+
+class Docente(models.Model):
+    nome = models.CharField(max_length=200)
+    email = models.EmailField(blank=True)
+    link_pagina = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.nome
