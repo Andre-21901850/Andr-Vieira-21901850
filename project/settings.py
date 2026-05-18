ac4230197eb4386aaf77f87317be1ae77ea3b5d1
Andre-21901850 <61785546+Andre-21901850@users.noreply.github.com>
2026-05-18 12:50:33 +0000

    Criada app portfolio, configurados settings e urls para media

--- project/settings.py
+++ project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'receita',
     'loja',
     'festivais',
+    'portfolio',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+# Default primary key field type
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+import os
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
